2745ed6b0263d6bcabf94c06d5a4419b7d156710
JuliaStrelkova <strelkovajuli@gmail.com>
2019-02-28 23:24:08 +0300

    cs-fix

--- src/Renderer.php
+++ src/Renderer.php
@@ -15,36 +15,45 @@ function render(?array $data, int $level = 1): string
         function ($acc, $item) use ($level) {
             if ($item['state'] === STATE_UNCHANGED) {
                 if (is_array($item['oldValue'])) {
-                    return $acc . renderIndentation($level) . $item['key'] . ': ' . render($item['oldValue'], $level + 1) . PHP_EOL;
+                    return $acc . renderIndentation($level) . $item['key']
+                        . ': ' . render($item['oldValue'], $level + 1) . PHP_EOL;
                 }
 
-                return $acc . renderIndentation($level) . $item['key'] . ': ' . renderScalarValue($item['oldValue']) . PHP_EOL;
+                return $acc . renderIndentation($level) . $item['key']
+                    . ': ' . renderScalarValue($item['oldValue']) . PHP_EOL;
             }
 
             if ($item['state'] === STATE_CHANGED) {
                 if (isset($item['oldValue'])) {
                     return $acc
-                        . renderIndentation($level, '  + ') . $item['key'] . ': ' . renderScalarValue($item['newValue']) . PHP_EOL
-                        . renderIndentation($level, '  - ') . $item['key'] . ': ' . renderScalarValue($item['oldValue']) . PHP_EOL;
+                        . renderIndentation($level, '  + ') . $item['key']
+                        . ': ' . renderScalarValue($item['newValue']) . PHP_EOL
+                        . renderIndentation($level, '  - ') . $item['key']
+                        . ': ' . renderScalarValue($item['oldValue']) . PHP_EOL;
                 }
 
-                return $acc . renderIndentation($level) . $item['key'] . ': ' . render($item['newValue'], $level + 1);
+                return $acc . renderIndentation($level) . $item['key']
+                    . ': ' . render($item['newValue'], $level + 1);
             }
 
             if ($item['state'] === STATE_DELETED) {
                 if (is_array($item['oldValue'])) {
-                    return $acc . renderIndentation($level, '  - ') . $item['key'] . ': ' . render($item['oldValue'], $level + 1) . PHP_EOL;
+                    return $acc . renderIndentation($level, '  - ') . $item['key']
+                        . ': ' . render($item['oldValue'], $level + 1) . PHP_EOL;
                 }
 
-                return $acc . renderIndentation($level, '  - ') . $item['key'] . ': ' . renderScalarValue($item['oldValue']) . PHP_EOL;
+                return $acc . renderIndentation($level, '  - ') . $item['key']
+                    . ': ' . renderScalarValue($item['oldValue']) . PHP_EOL;
             }
 
             if ($item['state'] === STATE_ADDED) {
                 if (is_array($item['newValue'])) {
-                    return $acc . renderIndentation($level, '  + ') . $item['key'] . ': ' . render($item['newValue'], $level + 1) . PHP_EOL;
+                    return $acc . renderIndentation($level, '  + ') . $item['key']
+                        . ': ' . render($item['newValue'], $level + 1) . PHP_EOL;
                 }
 
-                return $acc . renderIndentation($level, '  + ') . $item['key'] . ': ' . renderScalarValue($item['newValue']) . PHP_EOL;
+                return $acc . renderIndentation($level, '  + ') . $item['key']
+                    . ': ' . renderScalarValue($item['newValue']) . PHP_EOL;
             }
 
             throw new RuntimeException('Unexpected state value');
